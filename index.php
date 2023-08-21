506c0299201cd67cb189432206ba31a8c295b992
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-08-21 21:41:15 +0200

    Edit css

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //LINK BOOTSTRAP AND FILE CSS
+    <!-- LINK BOOTSTRAP AND FILE CSS -->
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
     <link rel="stylesheet" href="./css/style.css">
 
     <title>Bad words</title>
+    
 </head>
 <body>
     <!-- <div>
@@ -19,7 +20,7 @@
         </form>
     </div> -->
     <div class="container">
-        <h1 class="display-3 fw-bold text-center my-5">Bad Words</h1>
+        <h1 class="fw-bold text-center my-5">Bad Words</h1>
             <form action="paragrafo.php" method="POST">
                 <div class="mb-3">
                     <label class="form-label">Inserisci qui un paragrafo</label>
