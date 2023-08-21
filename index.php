4544bd807ee3ad4b09286b54d4e78c183b676bda
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-08-21 16:41:16 +0200

    add file php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$cognome = $_GET['cognome'];
+$nome = $_GET['nome'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Badwords</title>
+</head>
+<body>
+    <div>
+        <form action="saluto.php" method="GET">
+            <input type="text" name="nome">
+            <button type="submit">
+                INVIA
+            </button>
+        </form>
+    </div>
+</body>
+</html>
